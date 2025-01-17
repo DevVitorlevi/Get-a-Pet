23e51e02ca7e43c43d4cdcd85e71ee8acdc024b4
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-16 22:19:07 -0300

    Model do User Criado

---
+++ backend/models/User.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose
+
+const User = mongoose.model('User', new Schema({
+    nome:{
+        type:String,require:true
+    },
+    email:{
+        type:String,require:true
+    },
+    image:{
+        type:String
+    },
+    telefone:{
+        type:String,
+        require:true
+    },
+    senha:{
+        type:Number,require:true
+    },
+
+},
+    {timestamps:true}))
