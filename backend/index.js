6e74a2447baa96c3ca4096d87816bc3749c85f74
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-16 21:57:57 -0300

    modulos e configs feitas

---
+++ backend/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+// JSON
+app.use(express.json())
+
+//CORS
+
+app.use(cors({credentials:true, origin:'http://localhost:3000'}))
+
+// Public
+
+app.use(express.static('public'))
+
+// Routes
+
+app.listen(5000)
