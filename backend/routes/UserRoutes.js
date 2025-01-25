892e833b34bdbcc85bf6154491b338f060331724
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-25 14:29:46 -0300

    criando a rota para checar o usuario

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -3,5 +3,6 @@ const UserController = require('../controllers/UserController')
 
 router.post('/registrar', UserController.registrar)
 router.post('/login', UserController.login)
+router.get('/checkuser', UserController.checkUser)
 
 module.exports = router
