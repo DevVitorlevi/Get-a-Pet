5c4fd1b28fdf52aa16caace0cec25015d29498b0
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-24 21:48:58 -0300

    validações feitas

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -2,7 +2,26 @@ const User = require('../models/User')
 
 module.exports = class UserController {
     static async registrar(req,res){
-        res.json('Ola Get a Pet')
+        const {nome,email,telefone,senha,confirmesenha} = req.body
+
+        //Validações de Campos
+        if(!nome ||!email ||!telefone ||!senha ||!confirmesenha){
+            res.status(422).json({message:'Campo Obrigatório'})
+        }
+
+        if(senha !== confirmesenha){
+            res.status(422).json({message:'Senhas não coincidem'})
+        }
+
+        //Verifica se  usuário existe
+
+        const UserExist = await User.findOne({email:email})// se a variavel for preeechida é sinal que o usuário ja exista 
+
+        if(UserExist){
+            res.status(422).json({message:'Usuário Existe'})
+        }
+
+
     }
 }
 
