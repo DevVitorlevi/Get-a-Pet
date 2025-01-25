9fe72007f1247f7b5cde62f26ddc27e236aebe6b
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-25 14:29:23 -0300

    att

--- backend/helpers/create-token-user.js
+++ backend/helpers/create-token-user.js
@@ -5,7 +5,7 @@ const CreateUserToken = async (user, req, res) => {
     const token = jwt.sign({
         nome: user.nome, // Nome do usuário que será incluído no payload
         id: user._id     // ID único do usuário no banco de dados
-    }, 'secret');        // Chave secreta usada para criar a assinatura do token
+    }, 'nossosecret');        // Chave secreta usada para criar a assinatura do token
 
     res.status(200).json({
         message: 'Você está autenticado', // Mensagem informando que a autenticação foi bem-sucedida
