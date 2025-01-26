742d0a558f43e498d92b45a13099676c938f0414
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-25 22:24:08 -0300

    att

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
 const UserController = require("../controllers/UserController");
 
+const verifyToken = require('../helpers/verify-token')
+
 router.post("/registrar", UserController.registrar);
 router.post("/login", UserController.login);
 router.get("/checkuser", UserController.checkUser);
 router.get("/:id", UserController.getUser);
-router.patch('/edit/:id',UserController.editUser)
+router.patch('/edit/:id',verifyToken,UserController.editUser)
 module.exports = router;
