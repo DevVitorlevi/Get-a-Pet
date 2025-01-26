5c2f144e6ac22c19493ea319968f9d0c1db6c61b
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-01-25 22:24:57 -0300

    criado um helper que pega o user pelo token

---
+++ backend/helpers/get-user-by-token.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+
+const getUserbyToken = async (token)=>{
+    if(!token){
+        return  res.status(401).json({message:'Acesso Negado'})
+    }
+
+    const decoded = jwt.verify(token,'nossosecret')
+
+    const userid = decoded.id
+
+    const user = await User.findOne({_id:userid})
+
+    return user
+}
+
+module.exports = getUserbyToken
