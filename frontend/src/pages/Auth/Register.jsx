9a77f9c82157de5e6bd905e4993caa98ab9680c6
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-02-03 15:09:01 -0300

    Estrutura feita

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -10,14 +10,17 @@ const Register = () => {
                 <div className="inputs">
                     <input type="text" name='name' className='input' placeholder='Nome' required />
                     <User className='icon' />
+                    <span className="required">Nome Inválido</span>
                 </div>
                 <div className="inputs">
                     <input type="text" required name='email' className='input' placeholder='E-mail' />
                     <AtSign className='icon' />
+                    <span className="required">Nome Inválido</span>
                 </div>
                 <div className="inputs">
                     <input type="number" name='phone' required className='input' placeholder='Telefone' />
                     <Phone className='icon' />
+                    <span className='Telefone Inválido'></span>
                 </div>
                 <div className="className"></div>
                 <input type="password" name="password" className="input" required placeholder='Senha' />
