e94a90bef3265ae88a0c0b34ae436b967acd8758
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-02-02 23:14:23 -0300

    começãndo a header mobile

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer>
+            <p><span className='bold'>Vitor Levi</span>&copy;2025</p>
+        </footer>
+    )
+}
+
+export default Footer
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,19 +1,32 @@
 import React from 'react'
 import { NavLink, Link } from 'react-router-dom'
+import logo from '../assets/logo.png'
+import style from '../styles/Header.module.css'
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
 const Header = () => {
     return (
-        <header>
-            <Link to='/'>
-                <h1>Logo</h1>
-            </Link>
-            <nav>
-                <ul>
-                    <li><NavLink to='/register'>Crie sua Conta</NavLink></li>
-                    <li><NavLink to='/login'>Entre</NavLink></li>
+        <>
+            <header id={style.header}>
+                <Link to='/'>
+                    <img src={logo} id={style.img} />
+                </Link>
+                <div className={style.menu_desktop}>
+                    <nav id={style.nav}>
+                        <ul id={style.ul}>
+                            <li><NavLink to='/' className={(isActive) => isActive ? `${style.a} ${style.active}` : style.a}>Adotar</NavLink></li>
+                            <li><NavLink to='/login' className={(isActive) => isActive ? `${style.a} ${style.active}` : style.a}>Entre</NavLink></li>
+                            <li><NavLink to='/register' className={(isActive) => isActive ? `${style.a} ${style.active}` : style.a}>Crie sua Conta</NavLink></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className={style.mobile_menu}>
+                    <button id={style.mobile_btn}>
+                        <i className="fa_solid_fa_bars"></i>
+                    </button>
 
-                </ul>
-            </nav>
-        </header>
+                </div>
+            </header>
+        </>
     )
 }
 
