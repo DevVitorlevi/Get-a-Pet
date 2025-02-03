ea0d051eb60c60c947b53b31a2146fe729c78244
Vitor Levi <160429477+DevVitorlevi@users.noreply.github.com>
2025-02-03 13:58:30 -0300

    Estrutura Input

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import '../../styles/Form.css'
+import { Eye, EyeClosed, User, AtSign, Phone, LockIcon } from 'lucide-react'
 const Register = () => {
+    const [open, setOpen] = useState(false)
     return (
-        <div>Register</div>
+        <div className='container'>
+            <h1 className='title'>Registrar</h1>
+            <form className='form'>
+                <div className="inputs">
+                    <input type="text" name='name' className='input' placeholder='Nome' required />
+                    <User className='icon' />
+                </div>
+                <div className="inputs">
+                    <input type="text" required name='email' className='input' placeholder='E-mail' />
+                    <AtSign className='icon' />
+                </div>
+                <div className="inputs">
+                    <input type="number" name='phone' required className='input' placeholder='Telefone' />
+                    <Phone className='icon' />
+                </div>
+                <div className="className"></div>
+                <input type="password" name="password" className="input" required placeholder='Senha' />
+                <LockIcon className='icon' />
+                <div onClick={() => setOpen(!open)}>
+                    {open ? <EyeClosed className='icon' /> : <Eye size={32} className='icon' />}
+                </div>
+                <button type="submit">Cadastrar</button>
+            </form>
+        </div>
     )
 }
 
